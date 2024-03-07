7b0f651997ad210c99a8969f77d5e8e11048a17e
ITyiming <2844114018@qq.com>
2024-03-07 15:51:55 +0800

    sort函数练习

---
+++ 错误票据.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+int main() {
+    int N;
+    cin >> N;
+
+    vector<int> ids;
+
+    for (int i = 0; i < N; ++i) {
+        int num;
+        while (cin >> num) {
+            ids.push_back(num);
+            if (cin.get() == '\n') break;
+        }
+    }
+
+    // 对所有ID进行排序
+    sort(ids.begin(), ids.end());
+
+    int duplicateID = 0, missingID = 0;
+
+    // 找出重号和断号
+    for (int i = 1; i < ids.size(); ++i) {
+        if (ids[i] == ids[i - 1]) {
+            duplicateID = ids[i];
+        } else if (ids[i] > ids[i - 1] + 1) {
+            missingID = ids[i - 1] + 1;
+        }
+    }
+
+    cout << missingID << " " << duplicateID << endl;
+
+    return 0;
+}
+
